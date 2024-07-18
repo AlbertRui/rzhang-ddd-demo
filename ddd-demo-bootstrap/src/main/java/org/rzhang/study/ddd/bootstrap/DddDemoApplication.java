f6f75e66dfcc5b4099f5d0d56618441a5e07e541
rzhang <isruizhang@gmail.com>
2024-07-19 01:01:06 +0800

    rename application

--- ddd-demo-bootstrap/src/main/java/org/rzhang/study/ddd/bootstrap/DddDemoApplication.java
+++ ddd-demo-bootstrap/src/main/java/org/rzhang/study/ddd/bootstrap/DddDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DddDemoBootstrapApplication {
+public class DddDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DddDemoBootstrapApplication.class, args);
+        SpringApplication.run(DddDemoApplication.class, args);
     }
 
 }

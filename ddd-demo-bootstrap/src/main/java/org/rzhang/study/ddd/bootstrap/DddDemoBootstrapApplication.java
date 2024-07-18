840ff5ec9020fb92c6a47b61677fe8d4c67fac46
rzhang <isruizhang@gmail.com>
2024-07-19 00:52:26 +0800

    init ddd project

---
+++ ddd-demo-bootstrap/src/main/java/org/rzhang/study/ddd/bootstrap/DddDemoBootstrapApplication.java
@@ -0,0 +1,13 @@
+package org.rzhang.study.ddd.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddDemoBootstrapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddDemoBootstrapApplication.class, args);
+    }
+
+}
---
+++ ddd-demo-bootstrap/src/test/java/org/rzhang/study/ddd/bootstrap/DddDemoBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rzhang.study.ddd.bootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddDemoBootstrapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
